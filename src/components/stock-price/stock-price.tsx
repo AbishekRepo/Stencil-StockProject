dd0b5174d9b2b464d6b2101ef2b2fb9e17bf29cd
Abishek <abishekhttps@gmail.com>
2023-01-26 18:32:15 +0530

    fetched dummy data & added state to capture value in P

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from "@stencil/core";
+import { Component, h, State } from "@stencil/core";
 
 @Component({
     tag: 'ab-atock-price',
@@ -8,19 +8,31 @@ import { Component, h } from "@stencil/core";
 
 export class StockPrice {
 
+    @State() fetchedPrice: number = 0;
+
     onSumbitHandle(event: Event){
         event.preventDefault();
-        console.log('Submitted');
+        fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            console.log(+data['Global Quote']['05. price']);
+            this.fetchedPrice = +data['Global Quote']['05. price'];
+        })
+        .catch(error => {
+            console.log(error);
+        })
     }
 
     render() {
         return [
-            <form onSubmit={this.onSumbitHandle}>
+            <form onSubmit={this.onSumbitHandle.bind(this)}>
                 <input id='stock-symbol'/>
                 <button type='submit'>Fetch Data</button>
             </form>,
             <div>
-                <p>Price : {0}</p>
+                <p>Price : ${this.fetchedPrice}</p>
             </div>
         ]
     }
